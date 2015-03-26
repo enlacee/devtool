7f2d945996e943a33f78b04e43bba315b8f8f07f
= <detops.tk@gmail.com>
2015-03-25 22:36:34 -0500

    update atom.sh

---
+++ developer/atom.sh
@@ -0,0 +1,23 @@
+## commad basic:
+CTRL + SHIFT + M  : ver markdown como github
+CTRL+ SHIFT+P : helper shortkey
+
+## intall package : https://atom.io/packages
+# option 01 : and install by repo
+    cd ~/.atom/packages
+
+# option 02 : install by shell
+    apm install jshint
+
+
+    
+
+## plugin
+- emmet : html>head>body
+- fancy new file  : create file with : CTRL+ALT+N
+- JSHint
+
+
+
+# emmet
+emmet sintaxis : http://docs.emmet.io/abbreviations/syntax/
--- ide/netbeans.sh
+++ ide/netbeans.sh
@@ -4,5 +4,9 @@ CTRL + O => search to base of class and variables # good sublime
 CTRL + ALT + SHIF + O => go to simbols
 SHIF + ALT => ir a un archivo
 
-# PASAR DE UN LUGAR A OTRO HISTORIAL
-ALT + DIRECCIONAL (LEFT OR RIGHT)
+# pasar de un lugar a otro historial
+ALT + FLAG(LEFT|RIGTH)
+# encontrar archivo
+ALT + SHIFT + O 
+# buscar simbolo (class : property)
+ALT + CTRL + SHIFT + O
