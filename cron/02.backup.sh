640f91991aafc9038dc7bd818be59c203f93fa07
Anibal Copitan <anibal.copitan@altimea.com>
2016-02-17 14:48:52 -0500

    update cde

--- cron/02.backup.sh
+++ cron/02.backup.sh
@@ -3,19 +3,22 @@
 BASE="$HOME/Escritorio"
 ME_DATE=`date +%Y-%m-%d:%H:%M:%S`
 FOLDER_SYNC="$HOME/AnibalSync"
+N_COMPRESS="$BASE.$ME_DATE.tar"
 
 if [ -d $BASE ]
 then
-	N_COMPRESS="$BASE.$ME_DATE.tar"
 	tar -zcvf $N_COMPRESS $BASE
 	if [ -d $FOLDER_SYNC ]
 	then
-		echo ">>> directorio ya existe"
+		echo ">>> Directorio: $FOLDER_SYNC"
+		echo ">>> ya existe"
 	else
 		mkdir $FOLDER_SYNC
 	fi
 
 	mv $N_COMPRESS $FOLDER_SYNC
+	echo ">>> Comprimido: $N_COMPRESS"
+	echo ">>> En: $FOLDER_SYNC"
 else
 	echo "archivos no encontrado: $BASE"
 fi
