7042dcb5d39e7345043b5de17ea94297193780c0
Anibal C <detops.tk@gmail.com>
2016-02-26 01:18:55 -0500

    cron start apps *base*

---
+++ cron/04.start-apps.sh
@@ -0,0 +1,10 @@
+# ! /bin/bash
+# Ejecutar programas de preferencia
+#
+# google-chrome
+
+APP[0]='google-chrome'
+APP[1]='rescuetime' # medidor de productividad
+APP[2]='thunderbird'
+APP[3]='atom'
+APP[4]=''
